9ce6e62965e47ad33bc42689b7245d5cd4a20d72
Aaron Gershman <aegershman@gmail.com>
2021-07-03 00:19:20 -0500

    minor, add license headers for green build
    tiny polish description
    
    tiny polish, capitalize Terraform for consistency

--- src/main/java/org/openrewrite/terraform/search/FindResource.java
+++ src/main/java/org/openrewrite/terraform/search/FindResource.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.terraform.search;
 
 import lombok.EqualsAndHashCode;
@@ -8,7 +23,6 @@ import org.openrewrite.Recipe;
 import org.openrewrite.hcl.HclVisitor;
 import org.openrewrite.hcl.marker.HclSearchResult;
 import org.openrewrite.hcl.tree.Hcl;
-import org.openrewrite.hcl.tree.Label;
 import org.openrewrite.terraform.TerraformResource;
 
 import static org.openrewrite.Tree.randomId;
@@ -28,7 +42,7 @@ public class FindResource extends Recipe {
 
     @Override
     public String getDescription() {
-        return "Find a Terraform resource by resource type";
+        return "Find a Terraform resource by resource type.";
     }
 
     @Override
