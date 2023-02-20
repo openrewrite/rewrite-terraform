4da3b78471f04568340192f01af35ee4c81c91cd
Jonathan Schneider <jkschneider@gmail.com>
2023-02-20 15:27:25 -0500

    Standardize build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,22 +5,8 @@ plugins {
 group = "org.openrewrite.recipe"
 description = "Refactor Terraform. Automatically."
 
-val rewriteVersion = if(project.hasProperty("releasing")) {
-    "latest.release"
-} else {
-    "latest.integration"
-}
+val rewriteVersion = rewriteRecipe.rewriteVersion.get()
 
 dependencies {
-    compileOnly("org.projectlombok:lombok:latest.release")
-    annotationProcessor("org.projectlombok:lombok:latest.release")
-    implementation(platform("org.openrewrite:rewrite-bom:${rewriteVersion}"))
-    implementation("org.openrewrite:rewrite-hcl")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:latest.release")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:latest.release")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:latest.release")
-
-    testImplementation("org.openrewrite:rewrite-test:${rewriteVersion}")
-    testImplementation("org.assertj:assertj-core:latest.release")
+    implementation("org.openrewrite:rewrite-hcl:${rewriteVersion}")
 }
