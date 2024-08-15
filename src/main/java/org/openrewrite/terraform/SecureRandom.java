b00f4d392e1b984310eea9157458e298794ae160
Jonathan Schnéider <jkschneider@gmail.com>
2024-08-15 00:38:12 -0400

    Migrate to JSpecify from OpenRewrite JSR-305 meta-annotations (#35)
    Use this link to re-run the recipe: https://app.moderne.io/builder/gW3xaSLWV?organizationId=T3BlblJld3JpdGU%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/main/java/org/openrewrite/terraform/SecureRandom.java
+++ src/main/java/org/openrewrite/terraform/SecureRandom.java
@@ -17,13 +17,13 @@ package org.openrewrite.terraform;
 
 import lombok.EqualsAndHashCode;
 import lombok.Value;
+import org.jspecify.annotations.Nullable;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.Option;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
 import org.openrewrite.hcl.HclIsoVisitor;
 import org.openrewrite.hcl.tree.Hcl;
-import org.openrewrite.internal.lang.Nullable;
 
 import java.time.Duration;
 
